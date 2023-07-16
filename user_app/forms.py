d28c17bd485728b5c83bbc7c8ab35ba98a284058
Mahdi_f__P <mahdifallahtpishe80@gmail.com>
2023-07-16 20:47:02 +0330

    CHANGE & ADD : i'm added a new form for user login

--- user_app/forms.py
+++ user_app/forms.py
@@ -35,3 +35,21 @@ class RegisterForm(forms.Form):
             return confirm_password
 
         raise ValidationError('رمز عبور و تکرار رمز عبور درست نمیباشد')
+
+
+class LoginForm(forms.Form):
+    email = forms.EmailField(
+        label="ایمیل",
+        widget=forms.EmailInput(),
+        validators=[
+            validators.MaxLengthValidator(100),
+            validators.EmailValidator,
+        ],
+    )
+    password = forms.CharField(
+        label="رمز عبور",
+        widget=forms.PasswordInput(),
+        validators=[
+            validators.MaxLengthValidator(100)
+        ],
+    )
