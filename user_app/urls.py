127e394491279d79c9e77272332bc7a9fbed348f
Mahdi_f__P <mahdifallahtpishe80@gmail.com>
2023-07-16 22:44:02 +0330

    CHANGE & REFACTOR : i'm refactoring this file

--- user_app/urls.py
+++ user_app/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('register', views.RegisterView.as_view(), name="register-user-page"),
-    path('login', views.LoginView.as_view(), name="login-user-page"),
-    path('forgot-password', views.ForgetPasswordView.as_view(), name="forgot-password-page"),
-    path('reset-password', views.ResetPasswordView.as_view(), name="reset-password-page"),
-    path('logout', views.LogoutView.as_view(), name="logout"),
+    path('register/', views.RegisterView.as_view(), name="register-user-page"),
+    path('login/', views.LoginView.as_view(), name="login-user-page"),
+    path('forgot-password/', views.ForgetPasswordView.as_view(), name="forgot-password-page"),
+    path('reset-password/', views.ResetPasswordView.as_view(), name="reset-password-page"),
+    path('logout/', views.LogoutView.as_view(), name="logout"),
 ]
