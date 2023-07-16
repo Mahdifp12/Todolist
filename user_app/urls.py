067a7f84b182058efd415d8a59457be260ce4d99
Mahdi_f__P <mahdifallahtpishe80@gmail.com>
2023-07-16 21:46:41 +0330

    CHANGE & REFACTOR : i'm added a new URL path for logout

--- user_app/urls.py
+++ user_app/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('login', views.LoginView.as_view(), name="login-user-page"),
     path('forgot-password', views.ForgetPasswordView.as_view(), name="forgot-password-page"),
     path('reset-password', views.ResetPasswordView.as_view(), name="reset-password-page"),
+    path('logout', views.LogoutView.as_view(), name="logout"),
 ]
