cefc4c377fd0dd2161729beac6ba4ca2d850dcc0
Mahdi_f__P <mahdifallahtpishe80@gmail.com>
2023-07-16 16:36:59 +0330

    CHANGE & REFACTOR : changed file with DB settings

--- todo/apps.py
+++ todo/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class TodoConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
+    default_auto_field = 'django.db.sqlite.models.BigAutoField'
     name = 'todo'
