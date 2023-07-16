43887efb01994db637cf34ed2927e2867b6fcfba
Mahdi_f__P <mahdifallahtpishe80@gmail.com>
2023-07-16 16:57:46 +0330

    RENAME & CHANGE & CREATE : create extra file & change models.py file with create new model & renamed db.sqlite -> db.sqlite3

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+
 # Create your models here.
+
+class ItemTodo(models.Model):
+    title = models.CharField(max_length=250, verbose_name="عنوان")
+    description = models.TextField(verbose_name="متن توصیف")
+    completed = models.BooleanField(default=False, verbose_name="تکمیل شده")
+    created_date = models.DateTimeField(auto_now_add=True)
