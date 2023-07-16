da6c9db817b6d196ce3957a2b6e6e298363e7a00
Mahdi_f__P <mahdifallahtpishe80@gmail.com>
2023-07-16 18:20:04 +0330

    CHANGE & ADD : i'm added a some code for todo list

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import ItemTodo
+
 
 # Create your views here.
+
+class TodoListView(ListView):
+    model = ItemTodo
+    template_name = "todo/todo_list.html"
+    context_object_name = 'todos'
