b643d0b6dfb3ed602d81322605449e362a43e839
Mahdi_f__P <mahdifallahtpishe80@gmail.com>
2023-07-17 19:34:02 +0330

    CHANGE & ADD : i'm added a new URL path for todo complated page

--- todo/urls.py
+++ todo/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.TodoListView.as_view(), name="todo-list"),
     path('add-todo/', views.AddTodo.as_view(), name="add-todo-page"),
-    path('todo/<slug:slug>', views.TodoDetailView.as_view(), name="todo-detail-page")
+    path('todo/<slug:slug>', views.TodoDetailView.as_view(), name="todo-detail-page"),
+    path('todo/delete/<slug:slug>', views.TodoCompleted.as_view(), name="todo-completed-page"),
 ]
