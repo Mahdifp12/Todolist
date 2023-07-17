fe4f937eb72dab97779ff1f9791e835bc8a6d3e2
Mahdi_f__P <mahdifallahtpishe80@gmail.com>
2023-07-17 14:08:55 +0330

    CHANGE & ADD : i'm added a new atrr in Meta class for oredering & i'm added a new field for user with one to many relational

--- todo/models.py
+++ todo/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from user_app.models import User
 
 
 # Create your models here.
@@ -8,10 +9,12 @@ class ItemTodo(models.Model):
     description = models.TextField(verbose_name="متن توصیف")
     completed = models.BooleanField(default=False, verbose_name="تکمیل شده")
     created_date = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True, verbose_name="کاربر")
 
     class Meta:
         verbose_name = "آیتم تودو"
         verbose_name_plural = "آیتم های تودو"
+        ordering = ['created_date']
 
     def __str__(self):
         return self.title
